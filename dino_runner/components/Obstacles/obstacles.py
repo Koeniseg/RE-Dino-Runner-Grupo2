2bcadba8a7bb50a1ca6cb7f940de7629b5a839f6
EnriqueRodriguez21 <Matias.rodriguez@fundacion-jala.org>
2022-09-06 23:10:23 -0400

    Grupo - 2    Clase - 2
    Implementación de los obstáculos

---
+++ dino_runner/components/Obstacles/obstacles.py
@@ -0,0 +1,18 @@
+import random
+import pygame
+
+from dino_runner.components.Obstacles.obstacle import Obstacle
+
+
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 330
+
+
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 305
